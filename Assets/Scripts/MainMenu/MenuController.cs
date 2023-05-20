e3c8b638dc2d653c83ebc0ef83c1516e0aaf66d7
gvarys <johnnyzaev@gmail.com>
2023-05-20 19:35:05 +0300

    Input manager and pause in game is done!

--- Assets/Scripts/MainMenu/MenuController.cs
+++ Assets/Scripts/MainMenu/MenuController.cs
@@ -6,7 +6,7 @@ namespace MainMenu
     public class MenuController : MonoBehaviour
     {
         [SerializeField] private GameObject settingsUIObject;
-        
+
         public void LoadNextScene()
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
