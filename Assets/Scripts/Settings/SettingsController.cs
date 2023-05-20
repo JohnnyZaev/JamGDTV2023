b62adc7e6c5fb15c0b47a99bcddd411d8742fac9
gvarys <johnnyzaev@gmail.com>
2023-05-20 17:59:10 +0300

    Added music with mixer and settingsController.cs. Main menu is done!

--- Assets/Scripts/Settings/SettingsController.cs
+++ Assets/Scripts/Settings/SettingsController.cs
@@ -1,12 +1,40 @@
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.UI;
 
 namespace Settings
 {
     public class SettingsController : MonoBehaviour
     {
+        [SerializeField] private AudioMixer masterMixer;
         [SerializeField] private Slider volumeSlider;
         [SerializeField] private GameObject mainMenuUI;
+        [SerializeField] private GameObject settingsUI;
+
+        private const string MasterVolume = "MasterVolume";
+        private const float Multiplier = 20f;
+
+        private void Awake()
+        {
+            volumeSlider.onValueChanged.AddListener(SetMusicVolume);
+        }
+
+        private void Start()
+        {
+            volumeSlider.value = PlayerPrefs.GetFloat(MasterVolume);
+            SetMusicVolume(volumeSlider.value);
+        }
+
+        private void SetMusicVolume(float volume)
+        {
+            var logVolume = Mathf.Log10(volume) * Multiplier;
+            masterMixer.SetFloat(MasterVolume, logVolume);
+        }
+
+        private void OnDisable()
+        {
+            PlayerPrefs.SetFloat(MasterVolume, volumeSlider.value);
+        }
 
         public void BackToMainMenu()
         {
@@ -21,7 +49,7 @@ namespace Settings
         
         private void CloseSettings()
         {
-            gameObject.SetActive(false);
+            settingsUI.SetActive(false);
         }
     }
 }
