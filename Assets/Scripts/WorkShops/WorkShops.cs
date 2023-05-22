1f1f51d43151f2bb09fb44b38aa52ce3a87dae6d
god1385 <80105405+god1385@users.noreply.github.com>
2023-05-22 15:25:01 +0300

    Added Timeline
    Added TimeLine, which switches different workshops and invokes event, which does something

---
+++ Assets/Scripts/WorkShops/WorkShops.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class WorkShops : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> workshops;
+    [SerializeField] private UnityEvent onWorkshopChanged;
+    private GameObject _activeWorkshop;
+    void Start()
+    {
+        workshops = new List<GameObject>();
+
+        foreach(Transform child in transform)
+        {
+            if (child.tag == "WorkShop")
+            {
+                workshops.Add(child.gameObject);
+            }
+        }
+        _activeWorkshop = workshops[0];
+        _activeWorkshop.SetActive(true);
+    }
+
+    public void ChangeWorkShop(int index)
+    {
+        onWorkshopChanged.Invoke();
+        _activeWorkshop?.SetActive(false);
+        _activeWorkshop = workshops[index];
+        _activeWorkshop.SetActive(true);
+    }
+
+
+    // Delete After Adding OnWorkShopChanged Event is Assigned
+    public void Test()
+    {
+        Debug.Log("lol");
+    }
+
+    
+}
