ee74264335defeaf7f31bf23ce4454a6dd640e16
god1385 <80105405+god1385@users.noreply.github.com>
2023-05-26 16:43:00 +0300

    Update TestPuzzle.cs

--- Assets/Scripts/Puzzles/TestPuzzle.cs
+++ Assets/Scripts/Puzzles/TestPuzzle.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -10,21 +8,9 @@ namespace Puzzles
         public UnityEvent OnSuccess { get; set; }
         public UnityEvent OnFailure { get; set; }
 
-        // Start is called before the first frame update
-        void Start()
-        {
-
-        }
-
         void IPuzzle.Start()
         {
             OnSuccess.Invoke();
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-
         }
     }
 }
