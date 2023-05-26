a84131990b117789a5268675e22e966a9b96f5e2
ferundal <ferundal@gmail.com>
2023-05-26 13:56:30 +0400

    Isometric camera, dealayed rotations, isometric movement
    Camera is set up for ismetric viev and added at prefab.
    After moving ro the new direction plaer change rotation during some time
    Movments is udapted for isometric camera.

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -6,35 +6,47 @@ namespace Player
     public class PlayerMovement : MonoBehaviour
     {
         [SerializeField] private float playerSpeed = 5f;
+        [Tooltip("The turning speed in degrees")]
+        [SerializeField] private float rotationSpeed = 180.0f;
 
         private Rigidbody _playerRb;
         private Vector3 _playerVelocity;
-        private Vector3 _lookAtDirection;
+        private readonly Quaternion _rotationOffset = Quaternion.Euler(0f, 45, 0f);
+        private float _rotationSpeedFixedDelta;
+        private Quaternion _targetRotation;
 
         private void Awake()
         {
             _playerRb = GetComponent<Rigidbody>();
+            _rotationSpeedFixedDelta = rotationSpeed * Time.fixedDeltaTime;
         }
 
         private void FixedUpdate()
         {
             GetInputs();
             MovePlayer();
+            RotatePlayer();
         }
 
         private void GetInputs()
         {
             _playerVelocity.x = InputManager.Instance.HorizontalInput * playerSpeed;
-            _playerVelocity.y = _playerRb.velocity.y;
             _playerVelocity.z = InputManager.Instance.VerticalInput * playerSpeed;
+            _playerVelocity = _rotationOffset * _playerVelocity;
         }
 
         private void MovePlayer()
         {
-            _lookAtDirection = _playerVelocity.normalized;
-            _lookAtDirection.y = 0;
-            _playerRb.transform.LookAt(transform.position + _lookAtDirection);
             _playerRb.velocity = _playerVelocity;
+        }
+
+        private void RotatePlayer()
+        {
+            if (_playerVelocity.x != 0 && _playerVelocity.z != 0)
+            {
+                _targetRotation = Quaternion.LookRotation(_playerRb.velocity, Vector3.up);
+            }
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, _targetRotation, _rotationSpeedFixedDelta);
         }
     }
 }
