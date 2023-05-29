4fc6efed9bf3542b1ccfaa3be0cdb5bbcbfa2bbb
god1385 <80105405+god1385@users.noreply.github.com>
2023-05-29 17:18:32 +0300

    Added

--- Assets/Scripts/Gameplay/Interactions/Observer.cs
+++ Assets/Scripts/Gameplay/Interactions/Observer.cs
@@ -1,4 +1,7 @@
 using CameraController;
+using Dialogue;
+using Input;
+using UnityEditor.ShaderGraph.Serialization;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -7,22 +10,48 @@ namespace Gameplay.Interactions
     public class Observer : MonoBehaviour, IInteraction
     {
         [SerializeField] private GameObject sparkle;
-        [SerializeField] CamerSettings  cameraSettings;
+        [SerializeField] CamerSettings  cameraZoomSettings;
+        [SerializeField] CamerSettings cameraOriginPostion;
+        [SerializeField] GameObject uiTimeLine;
         public UnityEvent OnSuccess { get; set; }
         public UnityEvent OnFailure { get; set; }
 
         private CameraController.CameraController _cameraController;
+        private bool isZoomActive;
+        private Vector3 _cameraOriginPosition;
 
         private void Awake()
         {
             _cameraController = FindObjectOfType<CameraController.CameraController>();
         }
+
+        private void Update()
+        {
+           if (InputManager.Instance.LeftMouseButtonInput && isZoomActive && !DialogueController.Instance.IsDialogueRunning)
+            {
+                ExitZoom();
+            }
+        }
         void IInteraction.Start()
         {
+            OnSuccess.Invoke();
+            _cameraOriginPosition = _cameraController.transform.position;
             _cameraController.Move(sparkle.transform.position);
-            _cameraController.Zoom(cameraSettings.zoom, cameraSettings.zoomSpeed);
+            _cameraController.Zoom(cameraZoomSettings.zoom, cameraZoomSettings.zoomSpeed);
             _cameraController.Blur(true);
             sparkle.layer = 7;
+            uiTimeLine.SetActive(false);
+            isZoomActive = true;
+        }
+
+        public void ExitZoom()
+        {
+            _cameraController.Move(_cameraOriginPosition);
+            _cameraController.Zoom(cameraOriginPostion.zoom, cameraOriginPostion.zoomSpeed);
+            _cameraController.Blur(false);
+            sparkle.layer = 6;
+            uiTimeLine.SetActive(true);
+            isZoomActive = false;
         }
     }
 }
--- Assets/Scripts/WorkShops/AnimationWorkShopChange.cs
+++ Assets/Scripts/WorkShops/AnimationWorkShopChange.cs
@@ -8,12 +8,18 @@ namespace Workshops
         private int _index;
         public void SetIndex(int index)
         {
+            transform.gameObject.SetActive(true);
             _index = index;
         }
 
         public void ChangeWorkShop()
         {
             workShops.ChangeWorkShop(_index);
+        }
+
+        public void TurnOffPanel()
+        {
+            transform.gameObject.SetActive(false);
         }
     }
 }
